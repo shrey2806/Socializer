dc4dfe8572bc9bd03e6a2858b8aae2348d17bf83
Shrey Agarwal <shrey.ag2806@gmail.com>
2019-07-07 12:33:11 +0530

    Initialized Objects

--- app/src/main/java/com/example/shrey/socializer/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/shrey/socializer/Fragments/ChatsFragment.java
@@ -3,11 +3,16 @@ package com.example.shrey.socializer.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shrey.socializer.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -15,6 +20,13 @@ import com.example.shrey.socializer.R;
  */
 public class ChatsFragment extends Fragment {
 
+    private RecyclerView mChatList;
+
+    private DatabaseReference userDatabase,messageDatabase, convDatabase;
+
+    private View mMainView;
+
+    private String currentUserId;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -24,8 +36,29 @@ public class ChatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+
+        mMainView=inflater.inflate(R.layout.fragment_chats, container, false);
+
+        currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mChatList = mMainView.findViewById(R.id.chats_recycler_view);
+        mChatList.setHasFixedSize(true);
+      //  mChatList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        // adds the conv. to the top of the list;
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+        mChatList.setLayoutManager(linearLayoutManager);
+
+
+        userDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        userDatabase.keepSynced(true);
+        messageDatabase = FirebaseDatabase.getInstance().getReference().child("messages").child(currentUserId);
+        messageDatabase.keepSynced(true);
+        convDatabase = FirebaseDatabase.getInstance().getReference().child("Chat").child(currentUserId);
+        convDatabase.keepSynced(true);
+        return mMainView;
     }
 
 }
